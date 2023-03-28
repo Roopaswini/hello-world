6273c92a65b84072e5c65adc274588769e08cf79
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-28 18:54:51 +0530

    added app2 prog

---
+++ app2.java
@@ -0,0 +1,13 @@
+public class app2 {
+
+    public static void main(String[] args) {
+
+        String[] texts = {"one", "two", "three" };
+        
+        try{
+            System.out.println(texts[3]);
+        } catch(RuntimeException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
