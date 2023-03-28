17e1d599e35bf96a3c4ae88c61629a19f556b861
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-28 22:51:10 +0530

    added ac prog

---
+++ ac.java
@@ -0,0 +1,34 @@
+class   Java {
+    public void start() {
+        System.out.println("Learning java..");
+    }
+}
+
+interface Plant {
+    public void grow();
+}
+
+public class ac {
+    
+    public static void main(String[] args ) {
+
+        Java java1 = new Java() {
+            @Override public void start() {
+                System.out.println("Shopping...");
+            }
+        };
+
+        java1.start();
+        
+        Plant plant1 = new Plant() {
+
+            @Override
+            public void grow() {
+                System.out.println("Plant growing");
+            }
+        };
+
+        plant1.grow();
+    }
+}
+
