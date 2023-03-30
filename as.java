265221116592360d01ce575762a9c60ac4d8fa63
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-30 19:48:45 +0530

    added as prog

---
+++ as.java
@@ -0,0 +1,20 @@
+
+
+public class as {
+    
+    public static void main(String[] args) {
+        String[] word = new String[3];
+
+        word[0] = "Hello";
+        word[1] = "to";
+        word[2] = "u";
+
+        System.out.println(word[2]);
+
+        String[] fruits = {"apple", "banana", "kiwi"};
+        for(String fruit: fruits) {
+            System.out.println(fruit);
+        }
+
+    }
+}
