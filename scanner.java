f384078904d0c41bdee2c5bb8bd25968d2564116
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-30 14:30:12 +0530

    added scanner prog

---
+++ scanner.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class scanner {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        //String fileName = "C:/users/John/Desktop/example.txt";
+
+        String fileName = "example.txt";
+        File textfile = new File(fileName);
+
+        Scanner in = new Scanner(textfile);
+            int value = in.nextInt();
+            System.out.println("Read value: " + value);
+
+            int count = 2;
+            while(in.hasNextLine()) {
+                String line = in.nextLine();
+
+                System.out.println(count + ": " + line);
+                count++;
+                    
+            }
+        in.close();
+    }
+}
+ 
