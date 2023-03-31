fc0a872bfc7cdd76a95ab010c1ffa3b4ae22c76b
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-31 16:12:15 +0530

    added string prog

---
+++ string.java
@@ -0,0 +1,24 @@
+public class string {
+    
+    public static void main(String[] args) {
+
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "Bob";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "Bob");
+
+        System.out.println("My integer is: " + myInt);
+
+    
+
+    }
+}
