ae872b8de4e1a0916bf325eb3cba1ccf06230070
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-31 12:49:57 +0530

    added alphabets prog

---
+++ alphabets.java
@@ -0,0 +1,17 @@
+
+
+public class alphabets {
+    
+    public static void main(String[] args) {
+
+        char ch;
+
+        for(ch = 'D'; ch <= 'S'; ch++) {
+            if(ch != 1)
+            {
+                System.out.print(", ");
+            }
+            System.out.print(ch);
+        }
+    }
+}
