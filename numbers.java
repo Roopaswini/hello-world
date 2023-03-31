c66e76a2356fc4e2353f287b68e2c8a938194148
Roopaswini <roopaswini8055ganne@gmail.com>
2023-03-31 12:30:00 +0530

    added numbers.java

---
+++ numbers.java
@@ -0,0 +1,22 @@
+
+
+public class numbers {
+    
+    public static void main(String[] args) {
+
+        int a=100;
+        for(int i=1;i<=a;i++) 
+            {
+
+                if(i != 1)
+                {
+                    System.out.print(", ");
+                }
+                System.out.print(i);
+                
+            }
+        //System.out.println(i +",");
+        
+    }
+
+}   
