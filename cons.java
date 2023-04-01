b60d86dac4a4d79c6344b5aab6d7db4f6bfe544c
Roopaswini <roopaswini8055ganne@gmail.com>
2023-04-01 11:51:59 +0530

    added cons prog

---
+++ cons.java
@@ -0,0 +1,16 @@
+class  Machine {
+
+    public Machine() {
+        System.out.println("Constructor running!");
+    }
+
+    public Machine(String name) {
+    }
+}
+
+public class cons {
+    public static void main(String[] args) {
+        Machine machine1 = new Machine();
+    }
+
+}
